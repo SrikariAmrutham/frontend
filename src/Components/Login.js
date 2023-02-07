ff2dd718fbc3662917e73a23f2523cf262b0dfe5
Srikari Amrutham <amruthamsrikari@gmail.com>
2023-02-07 12:34:48 +0530

    commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Login(){
     return <>
-        <form action="">
+        <form className="loginform" action="">
         <div className="headingsContainer">
             <h2>Sign in</h2>
             <p>Sign in with your username and password</p>
@@ -10,12 +10,12 @@ export default function Login(){
 
         <div className="mainContainer">
             <label for="username">Your username</label>
-            <input type="text" placeholder="Enter Username" name="username" required/>
+            <input className="logininput" type="text" placeholder="Enter Username" name="username" required/>
 
             <br/><br/>
 
             <label for="pswrd">Your password</label>
-            <input type="password" placeholder="Enter Password" name="pswrd" required/>
+            <input className="logininput" type="password" placeholder="Enter Password" name="pswrd" required/>
 
             <div className="subcontainer">
                 <p className="forgotpsd"> <Link to="">Forgot Password?</Link></p>
